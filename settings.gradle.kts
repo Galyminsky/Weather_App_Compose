aa17fe1be7c9fb4f567b6271bbfa8ea008551a67
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-06 09:08:51 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Weather_App_Compose"
+include(":app")
+ 
