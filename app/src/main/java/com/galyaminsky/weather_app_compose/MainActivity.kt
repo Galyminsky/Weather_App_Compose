b47a9359a9e6475e9c68555a1e66aaabd5cabb3a
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-06 11:29:06 +0600

    #2

--- app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.galyaminsky.weather_app_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.galyaminsky.weather_app_compose.screens.MainScreen
 
 
 const val API_KEY = "eb001e1861e842a699b120840221012"
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MainScreen()
         }
     }
 }
--- app/src/main/java/com/galyaminsky/weather_app_compose/screens/MainScreen.kt
+++ app/src/main/java/com/galyaminsky/weather_app_compose/screens/MainScreen.kt
@@ -1,23 +1,52 @@
 package com.galyaminsky.weather_app_compose.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.galyaminsky.weather_app_compose.R
+import com.galyaminsky.weather_app_compose.ui.theme.Purple40
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreen () {
-        Image(
-            painter = painterResource(id = R.drawable.back_1),
-            contentDescription = "image",
-            modifier = Modifier.fillMaxSize()
-                .alpha(0.8f),
-            contentScale = ContentScale.Crop
-        )
+fun MainScreen() {
+    Image(
+        painter = painterResource(id = R.drawable.back_1),
+        contentDescription = "image",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.8f),
+        contentScale = ContentScale.Crop
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = Purple40
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 5.dp
+            ),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+
+        }
+
+    }
 }
