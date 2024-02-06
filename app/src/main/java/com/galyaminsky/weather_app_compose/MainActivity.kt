be7da12cd1b754329bd007505fcf4fdeddd3bf6e
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-06 09:59:07 +0600

    #2

--- app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.galyaminsky.weather_app_compose
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -15,14 +16,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.galyaminsky.weather_app_compose.ui.theme.Weather_App_ComposeTheme
+import org.json.JSONObject
+
+
+const val API_KEY = "eb001e1861e842a699b120840221012"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Lisakovsk", this)
                 }
             }
         }
@@ -42,7 +49,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(name: String, context: Context) {
     val state = remember {
         mutableStateOf("Unknown")
 
@@ -58,7 +65,7 @@ fun Greeting(name: String) {
             contentAlignment = Alignment.Center
         ) {
             Text(
-                text = "Temperature in $name!",
+                text = "Temperature in $name = ${state.value} C",
             )
         }
 
@@ -70,7 +77,7 @@ fun Greeting(name: String) {
         ) {
             Button(
                 onClick = {
-
+                    getResult(name, state, context)
                 }, modifier = Modifier
                     .padding(10.dp)
                     .fillMaxWidth()
@@ -82,10 +89,20 @@ fun Greeting(name: String) {
 
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Weather_App_ComposeTheme {
-        Greeting("Android")
-    }
+private fun getResult(city: String, state: MutableState<String>, context: Context) {
+    val url = "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$city&aqi=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        { response ->
+            val obj = JSONObject(response)
+            state.value =
+                obj.getJSONObject("current").getString("temp_c")
+        },
+        { error ->
+            Log.d("MyLog", error.toString())
+        }
+    )
+    queue.add(stringRequest)
 }
