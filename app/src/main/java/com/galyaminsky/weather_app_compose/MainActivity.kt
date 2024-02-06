b641514107e8cd9f56740dd412e4140ab553b54c
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-06 09:26:36 +0600

    #1

--- app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
@@ -3,13 +3,25 @@ package com.galyaminsky.weather_app_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.galyaminsky.weather_app_compose.ui.theme.Weather_App_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +42,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    val state = remember {
+        mutableStateOf("Unknown")
+
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Temperature in $name!",
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(
+                onClick = {
+
+                }, modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Refresh")
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
