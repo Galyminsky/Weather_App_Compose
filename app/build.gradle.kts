fed181cea3713d0ddcf685f68927df66e0dccb94
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-06 12:03:00 +0600

    #3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
+
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
--- app/src/main/java/com/galyaminsky/weather_app_compose/screens/MainScreen.kt
+++ app/src/main/java/com/galyaminsky/weather_app_compose/screens/MainScreen.kt
@@ -2,20 +2,29 @@ package com.galyaminsky.weather_app_compose.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.galyaminsky.weather_app_compose.R
 import com.galyaminsky.weather_app_compose.ui.theme.Purple40
 
@@ -45,7 +54,30 @@ fun MainScreen() {
             ),
             shape = RoundedCornerShape(10.dp)
         ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(top = 8.dp, start = 8.dp),
+                        text = "20 Jun 2024 13:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/night/122.png",
+                        contentDescription = "im_2",
+                        modifier = Modifier.size(35.dp)
+                            .padding(end = 8.dp, top = 8.dp)
 
+                    )
+                }
+            }
         }
 
     }
