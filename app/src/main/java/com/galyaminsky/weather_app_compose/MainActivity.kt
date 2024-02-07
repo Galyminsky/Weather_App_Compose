43785083d7f665d7fcce7664261aa175d3f1cecc
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-07 10:19:37 +0600

    2

--- app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/weather_app_compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.galyaminsky.weather_app_compose
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.galyaminsky.weather_app_compose.screens.MainCard
 import com.galyaminsky.weather_app_compose.screens.TabLayout
 
@@ -34,4 +38,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+private fun getData (city: String, context: Context) {
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY&q=$city&days=3&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest (
+        Request.Method.GET,
+        url,
+        {
+            response ->
+        },
+        {
+            error ->
+        }
+    )
 }
